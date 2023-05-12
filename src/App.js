9e96836a64ac55759558c6db9b4b73a9f0c9da3d
lunaveira <naveiralucia@gmail.com>
2023-05-12 15:49:21 -0300

    actualizacion

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+
+function App() {
+
+  return (
+
+    <Text style={{backgroundColor: 'red'}}> hola  </Text> 
+
+)};
+
+
+
+export default App;
