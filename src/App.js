473a1ef7daeddefd3cb18b712de5717af38ce528
Your Name <antonelladgacetua@gmail.com>
2023-05-13 21:37:44 -0300

    Nuevo

--- src/App.js
+++ src/App.js
@@ -10,18 +10,23 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+import Login from './screens/Login';
+
+const Stack = createStackNavigator();
 
 
-
-
-function App() {
-
+export default function App() {
   return (
-
-    <Text style={{backgroundColor: 'red'}}> hola  </Text> 
-
-)};
-
-
-
-export default App;
+    <NavigationContainer>
+    
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Login" component={Login} />
+    </Stack.Navigator>
+    
+    </NavigationContainer>
+  );
+}
