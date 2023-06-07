0adb7efc273519a12c4d50a6d048e69f8ee59d0e
lunaveira <naveiralucia@gmail.com>
2023-06-07 17:04:45 -0300

    7 de junio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import React from 'react';
 
+import {useAuth0, Auth0Provider} from 'react-native-auth0';
+
 import {
 
   StyleSheet,
@@ -12,9 +14,16 @@ import Navigation from './Navigation';
 
 
 export default function App() {
+
+  
+
   return (
 
+    <Auth0Provider domain='dev-ubrem7ljiuvp24yd.us.auth0.com' clientId='bC8ihap2XK9MYaaXlcpd9R9mg8lgSi9i' >
+
     <Navigation />
+
+    </Auth0Provider>
 
 
   
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,11 @@
 import { Text, Button, View, TouchableOpacity } from 'react-native';
 import HomeButton from '../components/HomeButton';
+import {useAuth0} from 'react-native-auth0';
 
 
 export default function Home({navigation}) {
+
+  const {authorize} = useAuth0();
 
 
   return (
@@ -17,13 +20,19 @@ export default function Home({navigation}) {
       <Text className='text-3xl text-center text-white mx-2.5' >¿Querés comprar entradas?</Text>
 
 
-      <HomeButton color='#FF3131' title='Continuar con Google' > </HomeButton>
+      <HomeButton color='#FFF' title='Continuar con Google' handler={() => authorize()}> </HomeButton>
+
+      { /*<Button onPress={() => authorize()} title="Log in" /> */ }
+
+      
 
 
       <Text className='text-3xl text-center pt-10 text-white' >¿Tenes una sala de cine?</Text>
   
 
       <HomeButton color='#FF3131' title='Iniciar sesion' handler={()=>navigation.navigate({name:'Login'})}></HomeButton>
+
+
 
       <TouchableOpacity onPress={() => navigation.navigate({name:'Register'})}>
         <Text className='text-xl text-center pt-10 text-white' >¿No tenes cuenta? Creala ahora</Text>
