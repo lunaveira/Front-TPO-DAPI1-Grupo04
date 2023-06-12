38aa79fb0b7ffc2065b75002f58d4bce44256879
lunaveira <naveiralucia@gmail.com>
2023-06-12 18:31:22 -0300

    completo conexion con back del login

--- src/screens/CreateNewBranch.js
+++ src/screens/CreateNewBranch.js
@@ -8,8 +8,6 @@ export default function CreateNewBranch({navigation}) {
         <View className=" items-center bg-gray-900 h-screen"> 
             <Text className="text-white"> Poner el logo de la app y el logo del perfil aca arriba</Text>
 
-    
-
         <TextInput className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-10 h-13 
         mx-2.5 text-base text-white text-center " 
         placeholder="Nombre de la nueva sucursal" placeholderTextColor="white" />
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -12,11 +12,30 @@ export default function Login({ navigation }) {
 
     try {
 
-      const response = await fetch("/api/auths", { body: JSON.stringify({ email, password }), headers: { 'Content-Type': 'application/json' }, method: "POST" })
+      console.log(email)
+      console.log(password)
+
+      const response = await fetch("https://backendmobile-production.up.railway.app/api/auths", 
+      { body: JSON.stringify({ email, password }), 
+      headers: { 'Content-Type': 'application/json' }, method: "POST" })
 
       const json = await response.json()
 
       console.log(json)
+
+      const storeData = async (value) => {
+        try {
+          await AsyncStorage.setItem('@token', json.token)
+        } catch (e) {
+          // saving error
+        }
+      }
+
+
+     
+
+
+
     }
 
     catch(err) {
@@ -28,14 +47,12 @@ export default function Login({ navigation }) {
 
   }
 
-
   return (
 
 
     <View className=" items-center bg-gray-900 h-screen">
 
       <TextInput value={email} onChangeText={(text) => setEmail(text)} className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-10 h-13 mx-2.5 text-base text-white text-center " placeholder="Ingresa tu email" placeholderTextColor="white" />
-
 
 
       <TextInput secureTextEntry value={password} onChangeText={(text) => setPassword(text)} className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-10 h-13 mx-2.5 text-base text-white text-center" placeholder="Ingresa tu contraseña" placeholderTextColor="white" />
@@ -46,7 +63,7 @@ export default function Login({ navigation }) {
       <Button onPress={() => navigation.navigate({ name: 'Main Owner' })} title='prueba'></Button>
 
       <TouchableOpacity onPress={() => navigation.navigate({ name: 'Forgot Password' })}>
-        <Text className="text-white mt-7 text-right "> ¿Olvidaste tu constraseña? </Text>
+        <Text className="text-white mt-7 text-right "> ¿Olvidaste tu contraseña? </Text>
       </TouchableOpacity>
 
 
