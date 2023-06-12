9ed07c4f9e08133b5b7bcefe308822883195ecd9
lunaveira <naveiralucia@gmail.com>
2023-06-12 14:49:49 -0300

    agregue formulario para crear sucursal

--- src/screens/CreateNewBranch.js
+++ src/screens/CreateNewBranch.js
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native'
+import {View, Text, TextInput} from 'react-native'
 import HomeButton from '../components/HomeButton';
 
 
@@ -8,9 +8,44 @@ export default function CreateNewBranch({navigation}) {
         <View className=" items-center bg-gray-900 h-screen"> 
             <Text className="text-white"> Poner el logo de la app y el logo del perfil aca arriba</Text>
 
-        
+    
 
-            <Text className="text-white mt-7 text-base"> Crear nueva sucursal formulario </Text>
+        <TextInput className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-10 h-13 
+        mx-2.5 text-base text-white text-center " 
+        placeholder="Nombre de la nueva sucursal" placeholderTextColor="white" />
+
+        <TextInput className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-10 
+        h-13 mx-2.5 text-base text-white text-center" 
+        placeholder="Pais / esto tiene q ser dropdown" placeholderTextColor="white" />
+
+        <TextInput className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-10 
+        h-13 mx-2.5 text-base text-white text-center" 
+        placeholder="Provincia / esto tiene q ser dropdown" placeholderTextColor="white" />
+
+        <TextInput className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-10 
+        h-13 mx-2.5 text-base text-white text-center" 
+        placeholder="Localidad" placeholderTextColor="white" />
+
+        <TextInput className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-10 
+        h-13 mx-2.5 text-base text-white text-center" 
+        placeholder="Calle" placeholderTextColor="white" />
+
+        <TextInput className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-10 
+        h-13 mx-2.5 text-base text-white text-center" 
+        placeholder="Altura" placeholderTextColor="white" />
+
+        <TextInput className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-10 
+        h-13 mx-2.5 text-base text-white text-center" 
+        placeholder="Precio de la funcion" placeholderTextColor="white" />
+
+        <TextInput className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-10 
+        h-13 mx-2.5 text-base text-white text-center" 
+        placeholder="Cerrado temporalmente / tiene q ser dropdown" placeholderTextColor="white" />
+
+
+        <View border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-10 h-13> 
+            <Text className="text-base text-white text-center"> Ingresa la imagen de tu sucursal </Text>
+        </View>
 
            
         </View>
