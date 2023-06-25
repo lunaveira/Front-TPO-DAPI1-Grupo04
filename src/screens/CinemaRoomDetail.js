e10c88b30b14b11d5dc173d311446048c30b8886
lunaveira <naveiralucia@gmail.com>
2023-06-25 14:42:18 -0300

    agregue form para crear funciones

--- src/screens/CinemaRoomDetail.js
+++ src/screens/CinemaRoomDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, ScrollView } from "react-native";
 import HomeButton from "../components/HomeButton";
 import { useNavigation } from "@react-navigation/native";
 
@@ -41,7 +41,7 @@ export default function CinemaRoomDetail() {
   };
 
   return (
-    <View className=" items-center bg-gray-900 h-screen">
+    <ScrollView contentContainerStyle={{ alignItems: 'center' }} style={{ backgroundColor: 'rgb(17 24 39)', flex: 1 }}>
       <View className="items-center bg-red-400 w-80 h-20 mt-28 rounded-lg ">
         <Text className="text-white text-center text-xl mt-4"> Cantidad de filas: </Text>
       </View>
@@ -61,6 +61,6 @@ export default function CinemaRoomDetail() {
       <View className="mb-5 mt-5">
         <HomeButton color="#FF3131" title="Ver funciones" handler={handlerFunciones} />
       </View>
-    </View>
+    </ScrollView>
   );
 }
--- src/screens/CreateFunction.js
+++ src/screens/CreateFunction.js
@@ -1,10 +1,74 @@
-import { View } from "react-native";
+import { View, Text, TextInput, Button, Image } from 'react-native'
+import HomeButton from '../components/HomeButton';
+import React, { useEffect, useState } from "react";
+import { launchImageLibrary } from 'react-native-image-picker';
+import { ScrollView } from 'react-native';
 
-export default function CreateFunction() {
-    return(
 
-        <View className=" items-center bg-gray-900 h-screen">
-            
-        </View>
+export default function CreateFunction({ navigation }) {
+
+    const [dia, setDia] = useState('');
+    const [horario, setHorario] = useState('');
+    const [idPelicula, setIdPelicula] = useState('');
+    const [idSala, setIdSala] = useState('');
+
+    const crearFuncion = async () => {
+        try {
+            const response = await fetch('https://backendmobile-production.up.railway.app/api/funciones', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    dia: dia,
+                    horario: horario,
+                    id_pelicula: idPelicula,
+                    id_sala: idSala,
+                }),
+            });
+
+            if (response.status === 201) {
+                const nuevaFuncion = await response.json();
+                console.log(nuevaFuncion); // Imprime la respuesta del servidor
+
+
+                navigation.navigate('Functions');
+            } else {
+                console.error('Error al crear la función');
+            }
+        } catch (error) {
+            console.error(error);
+
+        }
+    };
+
+
+    return (
+        <ScrollView className="px-5 bg-gray-900 h-screen" contentContainerStyle={{ alignItems: 'center', justifyContent: 'center' }}>
+
+            <TextInput value={dia}
+                onChangeText={(text) => setDia(text)} className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-5 h-13 
+        mx-2.5 text-base text-white text-center "
+                placeholder="Día" placeholderTextColor="white" />
+
+            <TextInput value={horario}
+                onChangeText={(text) => setHorario(text)} className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-5 
+        h-13 mx-2.5 text-base text-white text-center"
+                placeholder="Horario" placeholderTextColor="white" />
+
+            <TextInput value={idPelicula}
+                onChangeText={(text) => setIdPelicula(text)} className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-5
+        h-13 mx-2.5 text-base text-white text-center"
+                placeholder="Pelicula" placeholderTextColor="white" />
+
+            <TextInput value={idSala}
+                onChangeText={(text) => setIdSala(text)} className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-5
+        h-13 mx-2.5 text-base text-white text-center"
+                placeholder="Sala" placeholderTextColor="white" />
+
+            <Button title="Crear Funcion" onPress={crearFuncion} />
+
+        </ScrollView>
     );
+
 }
