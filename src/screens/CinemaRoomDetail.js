199e9e6a25aac32b33c6cb7562d63128dbd1483b
Garagithub <119074045+Garagithub@users.noreply.github.com>
2023-06-25 12:03:55 -0300

    edit,create,delete de cinemaroom pero con BR

--- src/screens/CinemaRoomDetail.js
+++ src/screens/CinemaRoomDetail.js
@@ -1,50 +1,66 @@
+import React from "react";
 import { View, Text, Button } from "react-native";
 import HomeButton from "../components/HomeButton";
 import { useNavigation } from "@react-navigation/native";
 
-
 export default function CinemaRoomDetail() {
+  const navigation = useNavigation();
 
-    const navigation = useNavigation();
+  const handlerEdit = () => {
+    navigation.navigate("Edit Cinema Room");
+  };
 
-    const handlerEdit = () => {
-        navigation.navigate("Edit Cinema Room");
-    };
+  const handlerFunciones = () => {
+    navigation.navigate("Functions");
+  };
 
-    const handlerFunciones= () => {
-        navigation.navigate("Functions");
-    };
+  const handleDeleteCinemaRoom = async () => {
+    try {
+      const response = await fetch("https://backendmobile-production.up.railway.app/:idsucursal/:cinema-room/deletecinemaroom", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        
+      });
 
-    return (
+      if (response.status === 200) {
+        // La sala se eliminó exitosamente
+        console.log("Sala eliminada");
+      } else if (response.status === 400) {
+        const errorMessage = await response.text();
+        console.error("Error al eliminar la sala:", errorMessage);
+      } else if (response.status === 404) {
+        console.error("Sala no encontrada");
+      } else {
+        console.error("Error al eliminar la sala");
+      }
+    } catch (error) {
+      console.error("Error al conectarse con el servidor:", error);
+    }
+  };
 
-        <View className=" items-center bg-gray-900 h-screen">
+  return (
+    <View className=" items-center bg-gray-900 h-screen">
+      <View className="items-center bg-red-400 w-80 h-20 mt-28 rounded-lg ">
+        <Text className="text-white text-center text-xl mt-4"> Cantidad de filas: </Text>
+      </View>
 
-            <View className="items-center bg-red-400 w-80 h-20 mt-28 rounded-lg ">
-                <Text className="text-white text-center text-xl mt-4"> Cantidad de filas: </Text>
-            </View>
+      <View className="items-center bg-red-400 w-80 h-20 rounded-lg mt-5 ">
+        <Text className="text-white text-center text-xl mt-4"> Cantidad de columnas: </Text>
+      </View>
 
-            <View className="items-center bg-red-400 w-80 h-20 rounded-lg mt-5 ">
-                <Text className="text-white text-center text-xl mt-4"> Cantidad de columnas: </Text>
-            </View>
+      <View className="mb-5 mt-5">
+        <HomeButton color="#FF3131" title="Editar sala" handler={handlerEdit} />
+      </View>
 
+      <View className="mb-5 mt-5">
+        <HomeButton color="#FF3131" title="Eliminar sala" handler={handleDeleteCinemaRoom} />
+      </View>
 
-
-            <View className="mb-5 mt-5">
-            <HomeButton color='#FF3131' title="Editar sala" handler={handlerEdit}  />
-            </View>
-            <View className="mb-5 mt-5">
-            <HomeButton color='#FF3131' title="Eliminar sala"   />
-            </View>
-
-            <View className="mb-5 mt-5">
-            <HomeButton color='#FF3131' title="Ver funciones" handler={handlerFunciones}  />
-            </View>
-
-
-
-
-        </View>
-
-
-    );
+      <View className="mb-5 mt-5">
+        <HomeButton color="#FF3131" title="Ver funciones" handler={handlerFunciones} />
+      </View>
+    </View>
+  );
 }
--- src/screens/CreateCinemaRoom.js
+++ src/screens/CreateCinemaRoom.js
@@ -1,10 +1,76 @@
-import { View } from "react-native";
+import {View, Text, TextInput, Button, Image} from 'react-native'
+import HomeButton from '../components/HomeButton';
+import React, { useEffect, useState } from "react";
+import { launchImageLibrary } from 'react-native-image-picker';
+import { ScrollView } from 'react-native';
 
-export default function CreateCinemaRoom() {
-    return(
 
-        <View className=" items-center bg-gray-900 h-screen">
-            
-        </View>
+export default function CreateNewCinemaRooom({navigation}) {
+
+    const [fila, setFila] = useState(0);
+    const [columna, setColumna] = useState(0);
+    const [numero_sala, setNurmeroSala] = useState(0);
+    
+
+    /*value={isTemporarilyClosed}
+        onChangeText={setIsTemporarilyClosed}*/
+ 
+
+  
+  
+
+  
+    const handleCreateCinemaRoom = async () => {
+      try {
+        const response = await fetch("https://backendmobile-production.up.railway.app/cinema-room/${id_sucursal}", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            fila: fila,
+            columna: columna,
+            numero_sala:numero_sala
+          }),
+        });
+  
+        if (response.status === 200) {
+          // La sucursal se creó exitosamente
+          navigation.navigate("CinemaRooms");
+        } else if (response.status === 400) {
+          const errorMessage = await response.text();
+          console.error("Error al crear la sala:", errorMessage);
+        } else {
+          console.error("Error al crear la sala");
+        }
+      } catch (error) {
+        console.error("Error al conectarse con el servidor:", error);
+      }
+    };
+  
+
+    return (
+      <ScrollView className="px-5 bg-gray-900 h-screen" contentContainerStyle={{alignItems:'center',justifyContent:'center'}}>
+          
+
+        <TextInput value={fila}
+        onChangeText={setFila} className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-5 h-13 
+        mx-2.5 text-base text-white text-center " 
+        placeholder="cantidad de filas" placeholderTextColor="white" />
+
+        <TextInput value={columna}
+        onChangeText={setColumna} className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-5 
+        h-13 mx-2.5 text-base text-white text-center" 
+        placeholder="Cantidad de columnas" placeholderTextColor="white" />
+
+        <TextInput value={numero_sala}
+        onChangeText={setNurmeroSala} className="border-white bg-slate-700 border-2 rounded-lg p-2 mb-4 w-96 mt-5
+        h-13 mx-2.5 text-base text-white text-center" 
+        placeholder="numero de sala" placeholderTextColor="white" />
+
+        <Button onPress={ handleCreateCinemaRoom} title='Crear sala'></Button>
+           
+        </ScrollView>
     );
+
 }
