326a2c2f07baf2e12a176d4bcc1befcbf5c348fb
lunaveira <naveiralucia@gmail.com>
2023-06-25 14:48:14 -0300

    hice la navegacion para crear una funcion

--- src/screens/Functions.js
+++ src/screens/Functions.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button, ScrollView } from 'react-native';
+import { View, Text, Button, ScrollView, TouchableOpacity } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
 const Functions = ({ navigation }) => {
@@ -8,7 +8,7 @@ const Functions = ({ navigation }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://backendmobile-production.up.railway.app/api/functions/5');
+        const response = await fetch('https://backendmobile-production.up.railway.app/api/funciones');
         const result = await response.json();
         setData(result.funciones);
       } catch (error) {
@@ -27,16 +27,21 @@ const Functions = ({ navigation }) => {
       {data && (
         <View>
           {data.map((funcion) => (
-            <View>
-              <View key={funcion.id} className="rounded-lg border-white place-items-start ml-2 border-2 w-720 h-12 mt-10">
-                <Text className="text-white text-base">{funcion.nombre_pelicula} - {funcion.dia}</Text>
-              </View>
 
-              <View key={funcion.id} className="rounded-lg border-white place-items-start ml-2 border-2 w-720 h-12 mt-10">
-                <Text className="text-white text-base">{funcion.nombre_pelicula}</Text>
-                <Text>Día: {funcion.dia}</Text>
-                <Text>Horario: {funcion.horario}</Text>
-              </View>
+            <View>
+              <TouchableOpacity onPress={() => navigation.navigate({ name: 'Functions Detail' })}>
+                <View key={funcion.id} className="rounded-lg border-white place-items-start ml-2 border-2 w-720 h-12 mt-10">
+                  <Text className="text-white text-base">{funcion.nombre_pelicula} - {funcion.dia}</Text>
+                </View>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={() => navigation.navigate({ name: 'Functions Detail' })}>
+                <View key={funcion.id} className="rounded-lg border-white place-items-start ml-2 border-2 w-720 h-12 mt-10">
+                  <Text className="text-white text-base">{funcion.nombre_pelicula}</Text>
+                  <Text>Día: {funcion.dia}</Text>
+                  <Text>Horario: {funcion.horario}</Text>
+                </View>
+              </TouchableOpacity>
 
             </View>
           ))}
--- src/screens/FunctionsDetail.js
+++ src/screens/FunctionsDetail.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import HomeButton from "../components/HomeButton";
 import { useNavigation } from "@react-navigation/native";
 
@@ -13,7 +13,7 @@ export default function FunctionDetail() {
 
     return (
 
-        <View className=" items-center bg-gray-900 h-screen">
+        <ScrollView className="px-5 bg-gray-900 h-screen" contentContainerStyle={{ alignItems: 'center', justifyContent: 'center' }}>
 
             <View className="rounded-lg border-white place-items-start ml-2 border-2 w-60 h-12 mt-10">
                 <Text className="text-white">Titulo</Text>
@@ -49,6 +49,6 @@ export default function FunctionDetail() {
             <HomeButton color='#FF3131' title="Editar" handler={handlerEdit} />
             <HomeButton color='#FF3131' title="Eliminar" />
 
-        </View>
+        </ScrollView>
     );
 }
