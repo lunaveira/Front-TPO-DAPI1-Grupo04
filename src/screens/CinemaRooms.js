e6050adeefbd758f47e86eac3bdc266acf9c2ae5
Garagithub <119074045+Garagithub@users.noreply.github.com>
2023-07-04 11:28:18 -0300

    kjk

--- src/screens/CinemaRooms.js
+++ src/screens/CinemaRooms.js
@@ -1,14 +1,44 @@
 import { View, Text, Button } from "react-native";
 import ListCinemaRoom from "../components/ListCinemaRoom";
 import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from 'react';
 
-export default function CinemaRooms() {
 
+export default function CinemaRooms({route}) {
+    const [cinemaRooms, setCinemaRooms] = useState([]);
+
+    const { id_sucursal } = route.params;
     const navigation = useNavigation();
 
     const handlePress = () => {
         navigation.navigate("Cinema Room Detail");
     };
+    useEffect(() => {
+        fetchData();
+      }, []);
+      const fetchData = async () => {
+        try {
+          const token_encriptado = await AsyncStorage.getItem('@token');
+          const idSocio = JSON.parse(atob(token_encriptado.split('.')[1]));
+          setToken(token_encriptado);
+    
+          const response = await fetch(`https://backendmobile-production.up.railway.app/${route.params.sucursalId}/cinema-room/getall`, {
+            headers: {
+              Authorization: `Bearer ${token_encriptado}`,
+            },
+          });
+    
+          if (response.ok) {
+            const result = await response.json();
+            setCinemaRooms([result]);
+            console.log(result);
+          } else {
+            console.log('Error al obtener el detalle de la sucursal');
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
 
     return (
 
@@ -24,7 +54,8 @@ export default function CinemaRooms() {
 
             <Text className="text-center text-lg text-white mt-5"> Tus salas </Text>
 
-            <ListCinemaRoom title="sala 1" handler={handlePress}/> 
+            <ListCinemaRoom cinemaRooms={cinemaRooms} handler={handlePress} />
+
 
 
 
