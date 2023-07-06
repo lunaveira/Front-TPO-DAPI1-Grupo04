9e91f071c2d7ff08bd29b4728e59c2c6e3ca140a
lunaveira <naveiralucia@gmail.com>
2023-07-06 16:58:51 -0300

    estuve tocando el cinema rooms para que traiga las salas creadas

--- src/components/ListCinemaRoom.js
+++ src/components/ListCinemaRoom.js
@@ -1,16 +1,17 @@
-import { View, Text, TouchableOpacity } from "react-native";
-
+import React from 'react';
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 
 export default function ListCinemaRoom(props) {
-
-    
-    return (
-
-        <TouchableOpacity onPress={() => props.handler()}>
-            <View className="rounded-lg border-white place-items-start ml-2 border-2 w-60 h-12 mt-10">
-                <Text className="text-white">{props.title}</Text>
-
-            </View>
+  return (
+    <ScrollView className="px-5 bg-gray-900 h-screen" contentContainerStyle={{ alignItems: 'center', justifyContent: 'center' }}>
+      {props.cinemaRooms.map((cinemaRoom) => (
+        <TouchableOpacity key={cinemaRoom.id} onPress={() => props.handler(cinemaRoom.numero_sala)}>
+          <View className="rounded-lg border-white place-items-start ml-2 border-2 w-60 h-12 mt-10">
+            <Text className="text-white">{cinemaRoom.numero_sala}</Text>
+          </View>
         </TouchableOpacity>
-    );
+      ))}
+    </ScrollView>
+  );
 }
+
