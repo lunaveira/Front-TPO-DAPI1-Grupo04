f7ecd31ca482b40129b9c9fe0c39899f3d4a465d
lunaveira <naveiralucia@gmail.com>
2023-07-09 12:23:24 -0300

    intente hacer el sign in de google

--- android/app/src/main/java/com/tpo/MainApplication.java
+++ android/app/src/main/java/com/tpo/MainApplication.java
@@ -10,6 +10,10 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -25,6 +29,9 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new RNGoogleSigninPackage());  // Agrega esta línea
+          packages.add(new FBSDKPackage());  // Agrega esta línea
+
           return packages;
         }
 
