6aa27340c95399a34f9e63bebf4c627aee4502c2
Your Name <antonelladgacetua@gmail.com>
2023-07-09 21:33:41 -0300

    creada buy tickets y modificado movie details pero puede fallar algo

---
+++ src/screens/BuyTickets.js
@@ -0,0 +1,12 @@
+import { Text, ScrollView, TextInput, View, Image } from "react-native"
+import { useState } from 'react';
+import MoviesGrid from "../components/MoviesGrid";
+
+
+export default function MovieDetails() {
+    return(
+    <ScrollView contentContainerStyle={{ alignItems: "center" }} style={{ backgroundColor: "rgb(17 24 39)", flex: 1 }}>
+
+    </ScrollView>
+    );
+}
