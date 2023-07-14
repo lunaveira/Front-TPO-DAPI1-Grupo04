3157211dea54185e28410c49f9abb4d887d5eed5
Garagithub <119074045+Garagithub@users.noreply.github.com>
2023-07-14 16:36:27 -0300

    sss

--- src/screens/MovieDetails.js
+++ src/screens/MovieDetails.js
@@ -1,23 +1,44 @@
 import { Text, ScrollView, View, Image } from "react-native";
 import HomeButton from "../components/HomeButton";
+import { useEffect, useState } from 'react';
 
-export default function MovieDetails({navigation, route}) {
-  const { mail,id_funcion } = route.params;
-  console.log("mail:",mail);
-  console.log("id_funcion:",id_funcion);
+export default function MovieDetails({ navigation, route }) {
+  const { mail, id_funcion } = route.params;
+  console.log("mail:", mail);
+  console.log("id_funcion:", id_funcion);
+  const [movieDetails, setMovieDetails] = useState(null);
+
+  useEffect(() => {
+    fetchMovieDetails();
+  }, []);
+
+  const fetchMovieDetails = async () => {
+    try {
+      const response = await fetch(`https://backendmobile-production.up.railway.app/api/functions/${id_funcion}/getbyid`);
+      if (response.ok) {
+        const data = await response.json();
+        setMovieDetails(data);
+      } else {
+        console.error('Error al obtener los detalles de la película:', response.status);
+      }
+    } catch (error) {
+      console.error('Error en la solicitud:', error);
+    }
+  };
+  console.log(movieDetails);
   return (
     <ScrollView contentContainerStyle={{ alignItems: "center" }} style={{ backgroundColor: "rgb(17 24 39)", flex: 1 }}>
       <View style={{ flexDirection: "row", alignItems: "center", marginTop: 5 }}>
 
         <View style={{ width: "80%", marginLeft: "5%" }}>
 
-          <Image source={require("../images/theDarkKnight.jpeg")} style={{ width: "100%", height: "45%", aspectRatio: 2/3, resizeMode: "contain" }} />
+          <Image source={{ uri: `data:image/jpeg;base64,${movieDetails?.imagen}` }} style={{ width: "100%", height: "45%", aspectRatio: 2/3, resizeMode: "contain" }} />
 
           <View className= 'flex-row-reverse'>
 
           <View className='flex-col ' style={{alignItems:'flex-end', marginTop: -180, marginLeft: 100}}>
             <View style={{ width: 80, height: 80, borderRadius: 45, backgroundColor: "white", marginBottom: 10}}> 
-            <Text className='text-sm text-black text-center mt-4'> Genero Accion</Text>
+            <Text className='text-sm text-black text-center mt-4'>{movieDetails?.genero}</Text>
             </View>
             <View style={{width: 80, height: 80, borderRadius: 45, backgroundColor: "white" }}> 
             <Text className='text-black text-sm text-center mt-4'>Duracion 120'</Text>
@@ -29,11 +50,10 @@ export default function MovieDetails({navigation, route}) {
           </View>
           
           <View style={{ flexDirection: "column", marginTop: 10 }}>
-            <Text style={{ color: "white", fontSize: 24, fontWeight: "bold" }}>El caballero oscuro</Text>
+            <Text style={{ color: "white", fontSize: 24, fontWeight: "bold" }}>{movieDetails?.titulo}</Text>
 
             <Text style={{ color: "white", fontSize: 16, textAlign: "left", marginTop: 10 }}>
-              Sinopsis de la película: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt justo ac ligula scelerisque varius.
-              Nullam in fringilla tellus.
+            Sinopsis de la película: {movieDetails?.descripcion}
             </Text>
 
             <Text style={{ color: "white", fontSize: 18, fontWeight: "bold", textAlign: "left", marginTop: 10 }}>
