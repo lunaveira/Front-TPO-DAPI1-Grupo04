8b10fd3e320b401e07809d3f055cc2948ee523c2
Garagithub <119074045+Garagithub@users.noreply.github.com>
2023-07-16 16:12:28 -0300

    ssss

--- src/screens/BuyTickets.js
+++ src/screens/BuyTickets.js
@@ -5,14 +5,12 @@ import { Picker } from "@react-native-picker/picker";
 import HomeButton from "../components/HomeButton";
 import { useEffect } from 'react';
 
+export default function MovieDetails({ navigation, route }) {
 
-
-export default function MovieDetails({ navigation,route }) {
-
-    const [branch, setBranch] = useState("Sucursal");
-    const [day, setDay] = useState("Dia");
-    const [amount, setAmount] = useState("Cantidad");
-    const { mail, id_funcion,movieDetails } = route.params;
+  const [branch, setBranch] = useState("Sucursal");
+  const [day, setDay] = useState("Dia");
+  const [amount, setAmount] = useState("Cantidad");
+  const { mail, id_funcion, movieDetails } = route.params;
   console.log("mail:", mail);
   console.log("id_funcion:", id_funcion);
   const [sucursales, setSucursales] = useState([]);
@@ -20,18 +18,18 @@ export default function MovieDetails({ navigation,route }) {
   useEffect(() => {
     fetchSucursales();
   }, []);
-  console.log("movies detail.",movieDetails);
+
+  console.log("movies detail.", movieDetails);
 
   const fetchSucursales = async () => {
     try {
-        console.log("titulo sin codear:",movieDetails.titulo);
+      console.log("titulo sin codear:", movieDetails.titulo);
       const titulo = encodeURIComponent(movieDetails.titulo);
-      console.log("titulo codeado?",titulo);
+      console.log("titulo codeado?", titulo);
       const response = await fetch(`https://backendmobile-production.up.railway.app/api/sucursales/${titulo}/getsucursalesbypelicula`);
       if (response.ok) {
         const data = await response.json();
         setSucursales(data);
-        
       } else {
         console.error('Error al obtener las sucursales:', response.status);
       }
@@ -40,107 +38,81 @@ export default function MovieDetails({ navigation,route }) {
     }
   };
 
-console.log("sucursales:",sucursales);
-const handleNext = () => {
+  console.log("sucursales:", sucursales);
+
+  const handleNext = () => {
+    const selectedSala = sucursales.find((sucursal) => sucursal.nombre === branch);
+    const id_sala = selectedSala ? selectedSala.id_sala : null;
     navigation.navigate('Select Seats', {
       mail,
       id_funcion,
       sucursal: branch,
       cantidad: amount,
       dia: day,
+      id_sala,
     });
   };
-    return (
 
-        <ScrollView contentContainerStyle={{ alignItems: "center" }} style={{ backgroundColor: "rgb(17 24 39)", flex: 1 }}>
-            <View style={{ flexDirection: "row", alignItems: "center", marginTop: 5 }}>
-
-                <View style={{ width: "80%", marginLeft: "5%" }}>
-
-                    <Image source={{ uri: `data:image/jpeg;base64,${movieDetails?.imagen}` }} style={{ width: "100%", height: "35%", aspectRatio: 2 / 3, resizeMode: "contain" }} />
-
-                    <View className='flex-row-reverse'>
-
-                        <View className='flex-col ' style={{ alignItems: 'flex-end', marginTop: -180, marginLeft: 100 }}>
-                            <View style={{ width: 80, height: 80, borderRadius: 45, backgroundColor: "white", marginBottom: 10 }}>
-                                <Text className='text-sm text-black text-center mt-4'>{movieDetails?.genero}</Text>
-                            </View>
-                            <View style={{ width: 80, height: 80, borderRadius: 45, backgroundColor: "white" }}>
-                                <Text className='text-black text-sm text-center mt-4'>Duracion 120'</Text>
-                            </View>
-
-
-                        </View>
-
-                    </View>
-
-                    <View style={{ flexDirection: "column", marginTop: 10 }}>
-                        <Text style={{ color: "white", fontSize: 24, fontWeight: "bold" }}>{movieDetails?.titulo}</Text>
-
-                        <Text style={{ color: "white", fontSize: 16, textAlign: "left", marginTop: 10 }}>
-                            Sinopsis de la película: {movieDetails?.descripcion}
-                        </Text>
-
-                        <Text style={{ color: "white", fontSize: 18, fontWeight: "bold", textAlign: "left", marginTop: 10 }}>
-                            Puntuación: 5.0
-                        </Text>
-
-                        <Picker
-                            style={{ height: 40, width: '90%', backgroundColor: 'gray', marginBottom: 10, marginTop: 20, color: 'white', borderRadius: 10 }}
-                            selectedValue={branch}
-                            onValueChange={(itemValue, itemIndex) => setBranch(itemValue)}
-
-
-                        >
-                            <Picker.Item label="Seleccionar sucursal" value="Sucursal" />
+  return (
+    <ScrollView contentContainerStyle={{ alignItems: "center" }} style={{ backgroundColor: "rgb(17 24 39)", flex: 1 }}>
+      <View style={{ flexDirection: "row", alignItems: "center", marginTop: 5 }}>
+        <View style={{ width: "80%", marginLeft: "5%" }}>
+          <Image source={{ uri: `data:image/jpeg;base64,${movieDetails?.imagen}` }} style={{ width: "100%", height: "35%", aspectRatio: 2 / 3, resizeMode: "contain" }} />
+          <View className='flex-row-reverse'>
+            <View className='flex-col ' style={{ alignItems: 'flex-end', marginTop: -180, marginLeft: 100 }}>
+              <View style={{ width: 80, height: 80, borderRadius: 45, backgroundColor: "white", marginBottom: 10 }}>
+                <Text className='text-sm text-black text-center mt-4'>{movieDetails?.genero}</Text>
+              </View>
+              <View style={{ width: 80, height: 80, borderRadius: 45, backgroundColor: "white" }}>
+                <Text className='text-black text-sm text-center mt-4'>Duracion 120'</Text>
+              </View>
+            </View>
+          </View>
+          <View style={{ flexDirection: "column", marginTop: 10 }}>
+            <Text style={{ color: "white", fontSize: 24, fontWeight: "bold" }}>{movieDetails?.titulo}</Text>
+            <Text style={{ color: "white", fontSize: 16, textAlign: "left", marginTop: 10 }}>
+              Sinopsis de la película: {movieDetails?.descripcion}
+            </Text>
+            <Text style={{ color: "white", fontSize: 18, fontWeight: "bold", textAlign: "left", marginTop: 10 }}>
+              Puntuación: 5.0
+            </Text>
+            <Picker
+              style={{ height: 40, width: '90%', backgroundColor: 'gray', marginBottom: 10, marginTop: 20, color: 'white', borderRadius: 10 }}
+              selectedValue={branch}
+              onValueChange={(itemValue, itemIndex) => setBranch(itemValue)}
+            >
+              <Picker.Item label="Seleccionar sucursal" value="Sucursal" />
               {sucursales.map((sucursal, index) => (
                 <Picker.Item key={index} label={sucursal.nombre} value={sucursal.nombre} />
               ))}
             </Picker>
-
-                        <Picker
-                            style={{ height: 40, width: '90%', backgroundColor: 'gray', marginBottom: 10, marginTop: 10, color: 'white', borderRadius: 10 }}
-                            selectedValue={amount}
-                            onValueChange={(itemValue, itemIndex) => setAmount(itemValue)}
-
-
-                        >
-                            <Picker.Item label="Seleccionar cantidad" />
-                            <Picker.Item label="1" value="1" />
-                            <Picker.Item label="2" value="2" />
-                            <Picker.Item label="3" value="3" />
-                            <Picker.Item label="4" value="4" />
-                        </Picker>
-
-                        <Picker
-                            style={{ height: 40, width: '90%', backgroundColor: 'gray', marginBottom: 10, marginTop: 10, color: 'white', borderRadius: 10 }}
-                            selectedValue={day}
-                            onValueChange={(itemValue, itemIndex) => setDay(itemValue)}
-
-                        >
-                             <Picker.Item label="Seleccionar día" value="Dia" />
+            <Picker
+              style={{ height: 40, width: '90%', backgroundColor: 'gray', marginBottom: 10, marginTop: 10, color: 'white', borderRadius: 10 }}
+              selectedValue={amount}
+              onValueChange={(itemValue, itemIndex) => setAmount(itemValue)}
+            >
+              <Picker.Item label="Seleccionar cantidad" />
+              <Picker.Item label="1" value="1" />
+              <Picker.Item label="2" value="2" />
+              <Picker.Item label="3" value="3" />
+              <Picker.Item label="4" value="4" />
+            </Picker>
+            <Picker
+              style={{ height: 40, width: '90%', backgroundColor: 'gray', marginBottom: 10, marginTop: 10, color: 'white', borderRadius: 10 }}
+              selectedValue={day}
+              onValueChange={(itemValue, itemIndex) => setDay(itemValue)}
+            >
+              <Picker.Item label="Seleccionar día" value="Dia" />
               {sucursales.map((sucursal, index) => (
                 <Picker.Item key={index} label={sucursal.dia} value={sucursal.dia} />
               ))}
             </Picker>
-
-
-
-                    </View>
-
-
-                </View>
-
-            </View>
-
-            <View className='mt-5'>
-
-                <HomeButton color='#FF3131' title='Siguiente' handler={handleNext} ></HomeButton>
-
-            </View>
-
-
-        </ScrollView>
-
-    );
+          </View>
+        </View>
+      </View>
+      <View className='mt-5'>
+        <HomeButton color='#FF3131' title='Siguiente' handler={handleNext}></HomeButton>
+      </View>
+    </ScrollView>
+  );
 }
--- src/screens/SelectSeats.js
+++ src/screens/SelectSeats.js
@@ -1,48 +1,87 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 
 const SelectSeats = ({ route }) => {
-  const { mail, id_funcion, sucursal, cantidad, dia } = route.params;
-  console.log("mail:",mail,"id_funcion:",id_funcion,"nombre sucursal:",sucursal,"Cantidad de asientos",cantidad,"dia:",dia);
+  const { mail, id_funcion, sucursal, cantidad, dia,id_sala } = route.params;
+  const [numRows, setNumRows] = useState(0);
+  const [numColumns, setNumColumns] = useState(0);
+  const [seats, setSeats] = useState([]);
+  const [selectedSeats, setSelectedSeats] = useState([]);
 
 
-  const numRows = 5; // Número de filas
-  const numColumns = 6; // Número de columnas
+  useEffect(() => {
+    fetchFilaColumna();
+  }, []);
 
-  // Inicializar la matriz de asientos
-  const initialSeats = Array(numRows)
-    .fill()
-    .map(() => Array(numColumns).fill(false));
-
-  const [seats, setSeats] = useState(initialSeats);
-
-  // Función para manejar el estado del asiento seleccionado
-  const handleSeatPress = (row, column) => {
-    const updatedSeats = [...seats];
-    updatedSeats[row][column] = !updatedSeats[row][column];
-    setSeats(updatedSeats);
+  const fetchFilaColumna = async () => {
+    try {
+      const response = await fetch(`https://backendmobile-production.up.railway.app/api/salas/${id_sala}/getfilasycolumnas`);
+      if (response.ok) {
+        const data = await response.json();
+        const rows = data.map((item) => item.fila);
+        const columns = data.map((item) => item.columna);
+        const maxRow = Math.max(...rows);
+        const maxColumn = Math.max(...columns);
+        setNumRows(maxRow);
+        setNumColumns(maxColumn);
+        initializeSeats(maxRow, maxColumn);
+      } else {
+        console.error('Error al obtener las filas y columnas:', response.status);
+      }
+    } catch (error) {
+      console.error('Error en la solicitud:', error);
+    }
   };
 
-  // Función para renderizar los asientos
+  const initializeSeats = (rows, columns) => {
+    const initialSeats = Array(rows)
+      .fill()
+      .map(() => Array(columns).fill(false));
+    setSeats(initialSeats);
+  };
+
+  const handleSeatPress = (row, column) => {
+    const seatNumber = row * numColumns + column + 1;
+  const seatIndex = selectedSeats.indexOf(seatNumber);
+
+  if (seatIndex !== -1) {
+    // Si el asiento ya está seleccionado, lo eliminamos de los asientos seleccionados
+    const updatedSeats = [...selectedSeats];
+    updatedSeats.splice(seatIndex, 1);
+    setSelectedSeats(updatedSeats);
+  } else {
+    // Si el asiento no está seleccionado y aún se pueden seleccionar más asientos
+    if (selectedSeats.length < cantidad) {
+      const updatedSeats = [...selectedSeats, seatNumber];
+      setSelectedSeats(updatedSeats);
+    }
+  }
+  };
+
   const renderSeats = () => {
     return seats.map((seatRow, rowIndex) => (
-      <View className='space-x-2'key={rowIndex} style={styles.seatRow}>
-        {seatRow.map((isSeatSelected, columnIndex) => (
-          <TouchableOpacity
-            key={columnIndex}
-            onPress={() => handleSeatPress(rowIndex, columnIndex)}
-            style={[
-              styles.seat,
-              isSeatSelected ? styles.selectedSeat : null,
-            ]}
-            activeOpacity={0.8}
-          >
-            <Text style={styles.seatText}>{`${rowIndex}${columnIndex}`}</Text>
-          </TouchableOpacity>
-        ))}
+      <View key={rowIndex} style={styles.seatRow}>
+        {seatRow.map((isSeatSelected, columnIndex) => {
+          const seatNumber = rowIndex * numColumns + columnIndex +1;
+          return (
+            <TouchableOpacity
+              key={columnIndex}
+              onPress={() => handleSeatPress(rowIndex, columnIndex)}
+              style={[
+                styles.seat,
+                isSeatSelected ? styles.selectedSeat : null,
+                selectedSeats.includes(seatNumber) ? styles.highlightedSeat : null,
+              ]}
+              activeOpacity={0.8}
+            >
+              <Text style={styles.seatText}>{seatNumber}</Text>
+            </TouchableOpacity>
+          );
+        })}
       </View>
     ));
   };
+  
 
   return (
     <ScrollView contentContainerStyle={{ alignItems: "center" }} style={{ backgroundColor: "rgb(17 24 39)", flex: 1 }}>
@@ -80,6 +119,9 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 5,
+    width: 40, // Ajusta el ancho del cuadrado de selección
+    height: 40, // Ajusta la altura del cuadrado de selección
+    marginHorizontal: 5, // Agrega una separación horizontal entre los asientos
   },
   selectedSeat: {
     backgroundColor: 'green',
@@ -88,6 +130,11 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 16,
   },
+  highlightedSeat: {
+    backgroundColor: 'green',
+  },
+  
 });
+
 
 export default SelectSeats;
